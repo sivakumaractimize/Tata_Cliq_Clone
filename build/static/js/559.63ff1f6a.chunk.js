2704f4bf5c01cc7e253f2761236f0cc65a902257
unknown <sivakumar@actimize.in>
2024-07-01 14:53:18 +0530

    thirdcommit

--- build/static/js/559.63ff1f6a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktatacliq=self.webpackChunktatacliq||[]).push([[559],{7453:(e,t,s)=>{s.r(t),s.d(t,{default:()=>n});s(5043);var a=s(4396),c=s(579);const n=()=>{const[e,t]=a.A.useState(!0);return a.A.useEffect((()=>{setTimeout((()=>{t(!1)}),1200)}),[]),(0,c.jsx)(c.Fragment,{children:e?(0,c.jsx)(a.A.Spinner,{}):(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(a.A.Navbar,{}),(0,c.jsx)(a.A.Myaccount,{}),(0,c.jsx)(a.A.Footer,{})]})})}}}]);
-//# sourceMappingURL=559.63ff1f6a.chunk.js.map
